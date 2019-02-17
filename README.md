b95523384af6d052cbf60bc69fe7b782a2715648
fedejeanne <2205684+fedejeanne@users.noreply.github.com>
2019-02-17 21:36:33 +0100

    Read intervals from input file
    ... and also switch to the develop branch

--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 ## Description
 ## How to run the app
 ### For developers
-Import the Maven project into your favorite IDE and run it with the following parameters.
-```
-TO-DO: take input parameters
-```
+Import the Maven project into your favorite IDE and run the `main(...)` method within the `com.daimler_tss.merge_task_fjeanne.Merger` class with the following parameters:
+* The path to a JSON file holding the list of intervals, like: `input/input1.txt`
 ### As a stand-alone Java app
-TO-DO: how to run it and parse the input list?
+In order to run the app from the command line, you need to run the following `maven` command:
+
+```
+mvn package exec:java -Dexec.args="input/input1.txt"
+```
+This will create the necessary JAR file and run the program using the file `input/input1.txt` as input (you can choose another one to try the app).
+
 ## Complexity of the algorithm
 See: [Big-O notation in Wikipedia](https://en.wikipedia.org/wiki/Big_O_notation)
 
--- src/main/java/com/daimler_tss/merge_task_fjeanne/Merger.java
+++ src/main/java/com/daimler_tss/merge_task_fjeanne/Merger.java
@@ -1,16 +1,91 @@
 package com.daimler_tss.merge_task_fjeanne;
 
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * @author fjeanne
  *
  */
 public class Merger {
+
+	/**
+	 * The program takes 1 (and only 1) String parameter:
+	 * <ul>
+	 * <li>A path to a JSON file with the input intervals (check the <i>input</i>
+	 * folder for some examples)</li>
+	 * </ul>
+	 * 
+	 * @param args use only 1 parameter with the input list of intervals.
+	 */
 	public static void main(String[] args) {
+
+		// TODO: remove all System.out/.err and use Log4J
+
+		// If there are no parameters, show the usage hint
+		if (args.length != 1) {
+			System.out.println(
+					"No input: run the program with exactly 1 parameter: the path to a text file with the input intervals.");
+			System.exit(0); // Not an error, just no input.
+		}
+
+		ObjectMapper mapper = new ObjectMapper();
+		ArrayList<Double> intervals = null;
+
+		// If the first parameter is a file, parse it
+		File inputFile = new File(args[0]);
+		if (inputFile.isFile()) {
+			try {
+				intervals = mapper.readValue(inputFile,
+						mapper.getTypeFactory().constructCollectionType(List.class, Point2D.Double.class));
+			} catch (JsonParseException | JsonMappingException e) {
+				System.err.println(
+						"Error parsing the intervals from the input file: '" + inputFile.getAbsolutePath() + "'");
+				e.printStackTrace();
+			} catch (IOException e) {
+				System.err.println("Error reading the input file: '" + inputFile.getAbsolutePath() + "'");
+				e.printStackTrace();
+			}
+		}
+
+		if (intervals == null) {
+			System.out.println("Couldn't parse the input, exiting.");
+
+			// Error
+			System.exit(1);
+		}
+
+		// input parsed correctly
+
+		try {
+			System.out.println("Input: " + mapper.writeValueAsString(intervals));
+		} catch (JsonProcessingException e) {
+			System.err.println("Couldn't print out the input in JSON format. Printing in raw (Java) format:");
+
+			System.out.println("Input: " + intervals);
+		}
+		Merger merger = new Merger();
+		merger.merge(intervals);
+
+		try {
+			System.out.println("Output: " + mapper.writeValueAsString(intervals));
+		} catch (JsonProcessingException e) {
+			System.err.println("Couldn't print out the output in JSON format. Printing in raw (Java) format:");
+
+			System.out.println("Output: " + intervals);
+		}
+
 	}
 
 	/**
